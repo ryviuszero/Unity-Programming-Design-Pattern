9edfd65d9af70b34d5f8669b7c87412766fd0136
mywind <ryviuszero@gmail.com>
2025-08-17 16:05:17 +0800

    add Strategy pattern

--- Assets/Patterns/SpecialAbilities/AbilityRunner.cs
+++ Assets/Patterns/SpecialAbilities/AbilityRunner.cs
@@ -2,28 +2,41 @@ using UnityEngine;
 
 public class AbilityRunner : MonoBehaviour
 {
-    enum Ability
-    {
-        FireBall,
-        Rage,
-        Heal
-    }
 
-    [SerializeField] private Ability currentAbility = Ability.FireBall;
+    [SerializeField] private IAbility currentAbility = new RageAbility();
 
     public void UseAbility()
     {
-        switch (currentAbility)
-        {
-            case Ability.FireBall:
-                Debug.Log("Casting FireBall!");
-                break;
-            case Ability.Rage:
-                Debug.Log("I'm always angry");
-                break;
-            case Ability.Heal:
-                Debug.Log("Here eat this!");
-                break;
-        }
+
+        currentAbility.Use();
+    }
+}
+
+public interface IAbility
+{
+    void Use(GameObject currentGameObject);
+}
+
+public class RageAbility : IAbility
+{
+    public void Use(GameObject currentGameObject)
+    {
+        Debug.Log("I'm always angry");
+    }
+}
+
+public class HealAbility : IAbility
+{
+    public void Use(GameObject currentGameObject)
+    {
+        Debug.Log("Here eat this!");
+    }
+}
+
+public class FireBallAbility : IAbility
+{
+    public void Use(GameObject currentGameObject)
+    {
+        Debug.Log("Casting FireBall!");
     }
 }
